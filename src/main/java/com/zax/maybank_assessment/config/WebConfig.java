83819078a534aa5ad49a87b6363b9cc975efce4f
zaxlee <zaxlee92@hotmail.com>
2025-08-17 10:37:47 +0800

    Revert "Merge pull request #5"
    This reverts commit 669ef40a4f3a0b5efe1e924486f002169ad2c6ca, reversing
    changes made to ed1ff42741643891e3d5f4eb82d5b230f67e7444.

--- src/main/java/com/zax/maybank_assessment/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.zax.maybank_assessment.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-@Configuration
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class WebConfig {
-}
